d169f102b6aa7016f9ea936aea47de4347023928
Domenic <uk095482@student.uni-kassel.de>
2025-11-15 15:10:18 +0100

    my own submit script

---
+++ slurmSubmitDomenic.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+####### Mail Notify / Job Name / Comment #######
+#SBATCH --job-name="broken.c"
+
+####### Partition #######
+#SBATCH --partition=pub23
+
+####### Ressources #######
+#SBATCH --time=0-00:05:00
+#SBATCH --mem-per-cpu=1000
+
+####### Node Info #######
+#SBATCH --exclusive
+#SBATCH --nodes=1
+
+####### Output #######
+#SBATCH --output=/home/users/0017/uk095482/Shared-Memory-PV/out/broken.out.%j
+#SBATCH --error=/home/users/0017/uk095482/Shared-Memory-PV/out/broken.err.%j
+
+export OMP_NUM_THREADS=4
+#cd /path/to/broken
+out/broken
