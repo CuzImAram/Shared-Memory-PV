d9baf57ab81888c508db65524a95da38d3602916
CuzImAram <aram.aytekin@gmail.com>
2025-11-22 12:50:23 +0100

    slurm

--- heatmap_analysis/slurmSubmitAram.sh
+++ heatmap_analysis/slurmSubmitAram.sh
@@ -17,4 +17,4 @@
 #SBATCH --output=/home/users/0019/uk097201/PV/Shared-Memory-PV/heatmap_analysis/out/heatmap_analysis.out.%j
 #SBATCH --error=/home/users/0019/uk097201/PV/Shared-Memory-PV/heatmap_analysis/out/heatmap_analysis.err.%j
 
-/home/users/0019/uk097201/PV/Shared-Memory-PV/heatmap_analysis/out/heatmap_analysis 10000 10000 123 0 100 55 0 48 1000
+/home/users/0019/uk097201/PV/Shared-Memory-PV/heatmap_analysis/out/heatmap_analysis 10000 10000 123 0 100 500 0 48 1000
